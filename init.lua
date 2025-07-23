6daf62d83e75403c55d638f8434dcbb885febfac
TomFardell <thomasfardell@outlook.com>
2025-07-23 23:44:25 +0100

    Add Rust LSP plugins

--- init.lua
+++ init.lua
@@ -494,6 +494,14 @@ require('lazy').setup({
       },
     },
   },
+
+  -- Rust extra LSP features
+  {
+    'mrcjkb/rustaceanvim',
+    version = '^6', -- Recommended
+    lazy = false, -- This plugin is already lazy
+  },
+
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
@@ -697,7 +705,7 @@ require('lazy').setup({
         -- pyright = {},
         pylsp = {},
         autopep8 = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
