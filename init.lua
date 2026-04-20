d7b9bc8258bcef34b68194502a4b524962545e76
TomFardell <thomasfardell@outlook.com>
2026-04-20 20:15:55 +0100

    Add languages to autoformat whitelist

--- init.lua
+++ init.lua
@@ -705,8 +705,10 @@ require('lazy').setup({
       format_on_save = function(bufnr)
         -- You can specify filetypes to autoformat on save here:
         local enabled_filetypes = {
-          -- lua = true,
-          -- python = true,
+          c = true,
+          cpp = true,
+          lua = true,
+          python = true,
         }
         if enabled_filetypes[vim.bo[bufnr].filetype] then
           return { timeout_ms = 500 }
